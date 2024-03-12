99d1fcfaf33b0dbb7ada9b9f6c2b554718606d40
filipmitrofan <72696960+filipmitrofan@users.noreply.github.com>
2024-03-12 11:45:56 +0200

    Create Subiecte.cpp

---
+++ Variante/Varianta_12/Subiecte.cpp
@@ -0,0 +1,42 @@
+Subiectul 1
+1.d
+2.
+a)17396
+b)3 2 1 0
+c)
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned int x;
+    cin>>x;
+    int y;
+    y=0;
+    while(x!=0)
+    {
+        while(x>9)
+        {
+            x/=10;
+        }
+        y=y*10+x;
+        cin>>x;
+    }
+    cout<<y;
+    return 0;
+}
+d)
+daca x!=0 atunci repeta.....pana cand(x=0)
+daca x>9 atunci repeta.....pana cand(x<=9)
+
+Subiectul 2
+1.b
+3.8-radacina; 2,3,4,5,6-descendenţii nodului 7
+4.11 *nf*rm*t*c*
+5.a[i][j]=(i*j)%10;
+Subiectul 3
+1.c
+2.2+2+2+3,2+2+5,2+7
+3.
+

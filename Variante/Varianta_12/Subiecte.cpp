98b44db2cebb36d70a1d2c79ef7d3b454796e9ea
filipmitrofan <72696960+filipmitrofan@users.noreply.github.com>
2024-03-12 12:01:11 +0200

    Update Subiecte.cpp

--- Variante/Varianta_12/Subiecte.cpp
+++ Variante/Varianta_12/Subiecte.cpp
@@ -1,8 +1,12 @@
 Subiectul 1
+
 1.d
+
 2.
 a)17396
+    
 b)3 2 1 0
+    
 c)
 #include <iostream>
 
@@ -26,17 +30,67 @@ int main()
     cout<<y;
     return 0;
 }
+
 d)
 daca x!=0 atunci repeta.....pana cand(x=0)
 daca x>9 atunci repeta.....pana cand(x<=9)
 
 Subiectul 2
+
 1.b
+
 3.8-radacina; 2,3,4,5,6-descendenţii nodului 7
+
 4.11 *nf*rm*t*c*
+
 5.a[i][j]=(i*j)%10;
+
 Subiectul 3
 1.c
-2.2+2+2+3,2+2+5,2+7
-3.
 
+2.2+2+2+3,2+2+5,2+7
+
+3.
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ifstream fin1("fin1.txt");
+ifstream fin2("fin2.txt");
+int main()
+{
+    int x,y,i,x1[101],vf[101]={0},j,n=0;
+    i=0;
+    while(fin1>>x)
+    {
+        if(x%5==0)
+        {
+            x1[i]=x;
+            i++;
+            n++;
+        }
+    }
+    while(fin2>>y)
+    {
+        if(y%5==0)
+        {
+            x1[i]=y;
+            i++;
+            n++;
+        }
+    }
+    for(i=0; i<=n; i++)
+    {
+        vf[x1[i]]++;
+    }
+    cout<<endl;
+
+    for(i=1; i<=61; i++)
+    {
+        if(vf[i]==1 && i!=0)
+            cout<<i<<" ";
+    }
+
+    return 0;
+}
+
+4.

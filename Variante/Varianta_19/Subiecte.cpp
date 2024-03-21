1df96ccedf04c21adfc62429c496f6c82dbf3913
filipmitrofan <72696960+filipmitrofan@users.noreply.github.com>
2024-03-21 08:19:58 +0200

    Create Subiecte.cpp

---
+++ Variante/Varianta_19/Subiecte.cpp
@@ -0,0 +1,26 @@
+Subiectul 1
+1.a
+2 a)2,3,4
+  b)312,1343/312,1342
+  c)#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,i;
+    cin>>a>>b;
+    a=((a/10)%10)*10+a%10;
+    b=((b/10)%10)*10+b%10;
+    for(i=a;i<=b;i++)
+        if(i/10==i%10)
+        cout<<i%10;
+    return 0;
+}
+d)i=a repeta pana cand i>b
+  
+  Subiectul 2
+1.
+3.bac20092009
+4.(b.x-a.x)/(b.y-a.y)==(b.x-a.0)/(b.y-a.0)
+5.

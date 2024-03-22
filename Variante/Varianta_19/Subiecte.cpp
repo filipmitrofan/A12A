18a961606f96ee90b2c9c5602330fea710e2d481
filipmitrofan <72696960+filipmitrofan@users.noreply.github.com>
2024-03-22 09:52:59 +0200

    Update Subiecte.cpp

--- Variante/Varianta_19/Subiecte.cpp
+++ Variante/Varianta_19/Subiecte.cpp
@@ -20,7 +20,58 @@ int main()
 d)i=a repeta pana cand i>b
   
   Subiectul 2
-1.
+1.a
 3.bac20092009
-4.(b.x-a.x)/(b.y-a.y)==(b.x-a.0)/(b.y-a.0)
-5.
+4.a.y==b.y
+5.if(i%2==1) a[i][j]=i+j;
+else a[i][j]=a[i-1][j-1];
+
+
+
+
+Subiectul 3
+1.c
+2.10349,10365,10367
+3.if(x[i]<0)
+swap(x[i],x[i+1])
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ifstream f("date1.in");
+ifstream g("date2.in");
+int main()
+{
+    int n,m,x,y,i,j;
+    f>>n;
+    g>>m;
+    i=j=1;
+    f>>x;
+    g>>y;
+    while(i<=n && j<=m)
+    {
+        if(x<y) 
+            { cout<<x<<" ";
+            f>>x;
+            i++;}
+        else if(y<x) 
+            { cout<<y<<" ";
+            g>>y;
+            j++;} 
+        else { cout<<x<<" ";
+                f>>x;
+                g>>y;
+                i++;
+                j++; } 
+    }
+        while(i<=n) 
+            { cout<<x<<" ";
+            f>>x;
+            i++;} 
+        while(j<=m)     
+            { cout<<y<<" ";
+            g>>y;
+            j++;} 
+        
+    return 0;
+}
